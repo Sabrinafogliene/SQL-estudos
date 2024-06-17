3c0cf5d529487b30da44ab0381445e52d17fc10f
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-06-17 18:19:26 -0300

    Update Primeira tabela.sql

--- Primeira tabela.sql
+++ Primeira tabela.sql
@@ -1,13 +1,5 @@
-create table if not exists teste(
-id int,
-nome varchar(10),
-idade int
-);
-insert into teste value
-('1', 'Pedro', '22'),
-('2', 'Maria', '12'),
-('3', 'Maricota', '77');
+delete from cursos
+where ano = '2018'
+limit 2;
 
-select * from teste;
-
-drop table if exists teste;
+select * from cursos;
