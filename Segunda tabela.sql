5e41b23ddf8f89407aed72d2f4c7d633022c2781
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-06-17 21:17:38 -0300

    Create Segunda tabela.sql

---
+++ Segunda tabela.sql
@@ -0,0 +1,19 @@
+use cadastro;
+select * from gafanhotos;
+select ano, nome, carga from cursos
+order by ano, nome;
+
+select nome, descricao, ano from cursos
+where ano != 2015
+order by ano;
+
+select nome, ano from cursos
+where ano between 2014 and 2016
+order by ano desc, nome;
+
+select nome, descricao, ano from cursos
+where ano in (2014, 2016, 2020)
+order by ano;
+
+select nome, carga, totaulas from cursos
+where carga > 35 or totaulas < 30;
