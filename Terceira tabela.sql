e8afc6cf23ee318e114f425a0f1ea7af5f3a5959
Sabrinafogliene <sabrina.castilho4@gmail.com>
2024-06-19 21:37:01 -0300

    Update Terceira tabela.sql

--- Terceira tabela.sql
+++ Terceira tabela.sql
@@ -27,3 +27,27 @@ on c.idcurso = g.cursopreferido;
 select g.nome, c.nome, c.ano
 from gafanhotos as g right outer join cursos as c
 on c.idcurso = g.cursopreferido;
+
+create table gafanhoto_assiste_curso (
+id int not null auto_increment,
+data date,
+idgafanhoto int,
+idcurso int,
+primary key (id),
+foreign key (idgafanhoto) references gafanhotos(id),
+foreign key (idcurso) references cursos(idcurso)
+) default charset = utf8mb4;
+
+describe gafanhoto_assiste_curso;
+insert into gafanhoto_assiste_curso values
+(default, '2014-01-01', '1', '2');
+
+select * from gafanhoto_assiste_curso;
+
+select g.nome, c.nome from gafanhotos g
+join gafanhoto_assiste_curso a
+on g.id = a.idgafanhoto
+join cursos c
+on c.idcurso = a.idcurso
+order by g.nome;
+
